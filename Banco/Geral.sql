826cbbf7914164cbcfb50669c462a6d32a85de62
Raphael Barbosa Rodrigues <42044496+RaphaelBRodrigues@users.noreply.github.com>
2019-08-07 13:34:21 -0300

    Create userTable

--- Banco/Geral.sql
+++ Banco/Geral.sql
@@ -1,1 +1,14 @@
 create database ONG;
+use ONG;
+
+create table usuario(
+AT_UserID int primary key auto_increment,
+AT_Name varchar(900),
+AT_Username varchar(40),
+AT_Password varchar(100),
+AT_Cep int,
+AT_Bairro varchar(40),
+AT_Street varchar(40),
+AT_PhoneOne int(11),
+AT_PhoneTwo int(11)
+);
