77b5cb2161cc269318ca9cd8fce4542796481eea
Raphael Barbosa Rodrigues <42044496+RaphaelBRodrigues@users.noreply.github.com>
2019-08-07 13:24:55 -0300

    Link cursos php

--- Views/Cursos.php
+++ Views/Cursos.php
@@ -8,41 +8,48 @@ include('../Models/Header.php');
 </div>
 <ul class='cursos'>
   Profissionalizante
-  <li><a href="#"> Eletricista </a></li>
-  <li><a href="#"> Cabeleleiro </a></li>
+  <li><a href="InfCursos.php?.php?id="> Eletricista </a></li>
+  <li><a href="InfCursos.php?.php?cursoid=2"> Cabeleleiro </a></li>
+  <li><a href="InfCursos.php?cursoid=3"> Eletricista </a></li>
+  <li><a href="InfCursos.php?cursoid=4"> Cabeleleiro </a></li>
+  <li><a href="InfCursos.php?cursoid=5"> Eletricista </a></li>
+  <li><a href="InfCursos.php?cursoid=6"> Cabeleleiro </a></li>
+
 </ul>
 
 
 <ul class='cursos'>
   Arte
-  <li><a href="#"> Violão </a></li>
-  <li><a href="#"> Teclado </a></li>
-  <li><a href="#"> Balé </a></li>
+  <li><a href="InfCursos.php?cursoid=7"> Violão </a></li>
+  <li><a href="InfCursos.php?cursoid=8"> Teclado </a></li>
+  <li><a href="InfCursos.php?cursoid=9"> Balé </a></li>
 
 </ul>
 
 <ul class='cursos'>
-  ????
-  <li><a href="#"> ???? </a></li>
+  ??
+  <li><a href="InfCursos.php?cursoid=10"> ?? </a></li>
 </ul>
 
 
 <ul class='cursos'>
-  ????
-  <li><a href="#"> ???? </a></li>
+  ??
+  <li><a href="InfCursos.php?cursoid=11"> ?? </a></li>
 </ul>
 
 <ul class='cursos'>
-  ????
-  <li><a href="#"> ???? </a></li>
+  ??
+  <li><a href="InfCursos.php?cursoid=12"> ?? </a></li>
 </ul>
 
 <ul class='cursos'>
-  ????
-  <li><a href="#"> ???? </a></li>
+  ??
+  <li><a href="InfCursos.php?cursoid=1"> ?? </a></li>
 </ul>
 </div>
 
+<br><br><br>
+<br><br><br>
 <br><br><br>
 
 <?php
